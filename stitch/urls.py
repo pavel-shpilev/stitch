23382b84a2806bd3069d5acaa9a2d5bb05b0fe7c
Pavel Shpilev <p.shpilev@gmail.com>
2015-03-08 21:16:13 +1000

    Members endpoint + renaming support

--- stitch/urls.py
+++ stitch/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from taskboard.views import BoardList, BoardView, LabelView
+from taskboard.views import BoardList, BoardView, LabelView, MemberList, MemberView
 
 
 urlpatterns = [
     url(r'^boards/$', BoardList.as_view()),
     url(r'^board/(?P<pk>[0-9]+)/$', BoardView.as_view()),
     url(r'^label/(?P<pk>[0-9]+)/$', LabelView.as_view()),
+    url(r'^members/$', MemberList.as_view()),
+    url(r'^member/(?P<pk>[0-9]+)/$', MemberView.as_view()),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
--- taskboard/views.py
+++ taskboard/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
 from rest_framework.response import Response
 
-from taskboard.models import Board, Label
-from taskboard.serializers import BoardListSerializer, BoardSerializer, LabelSerializer
+from taskboard.models import Board, Label, Member
+from taskboard.serializers import BoardListSerializer, BoardSerializer, LabelSerializer, \
+    MemberListSerializer, MemberSerializer
 
 
 class BoardList(generics.ListCreateAPIView):
@@ -16,7 +17,7 @@ class BoardView(generics.RetrieveUpdateDestroyAPIView):
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
-        # Boards can only be renamed.
+        # Boards can only be renamed. Ignore any other data (i.e. is_archived)
         data = {'name': request.data.get('name', instance.name)}
         serializer = self.get_serializer(instance, data=data, partial=True)
         serializer.is_valid(raise_exception=True)
@@ -30,8 +31,27 @@ class LabelView(generics.RetrieveUpdateAPIView):
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
-        # Labels can only be renamed.
+        # Labels can only be renamed. Ignore any other data (i.e. board)
         data = {'title': request.data.get('title', instance.title)}
+        serializer = self.get_serializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+
+class MemberList(generics.ListCreateAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberListSerializer
+
+
+class MemberView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        # Members can only be renamed. Ignore any other data (i.e. task list)
+        data = {'name': request.data.get('name', instance.name)}
         serializer = self.get_serializer(instance, data=data, partial=True)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
